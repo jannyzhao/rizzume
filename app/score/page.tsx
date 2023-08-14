2daf8062d6aafb042d4b0768ac96b3d2284ab7b8
Jan Clarin <me@janclarin.com>
2023-08-13 23:24:27 -0700

    Read text from pdf

--- app/score/page.tsx
+++ app/score/page.tsx
@@ -1,11 +1,17 @@
 import { Button, Container, Input } from "@mui/material";
 
-// Placeholder page.
+// Placeholder page for testing the route.
 export default function ScorePage() {
   return (
     <Container>
       <h1>Score</h1>
-      <form action="/api/score" method="post" encType="multipart/form-data">
+      <form
+        action="/api/score"
+        className="flex flex-col"
+        method="post"
+        encType="multipart/form-data"
+      >
+        <Input name="jobDescription" type="textarea" required />
         <Input name="resume" type="file" required />
         <Button type="submit">Upload</Button>
       </form>

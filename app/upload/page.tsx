9d29c06affab3cec274003ea442505e786c91caa
Nishant Racherla <nishant.racherla@gmail.com>
2023-08-13 20:42:48 -0700

    added upload form

---
+++ app/upload/page.tsx
@@ -0,0 +1,11 @@
+import UploadForm from "@/components/UploadForm";
+
+const Upload = () => {
+  return (
+    <div className="h-screen">
+      <UploadForm />
+    </div>
+  );
+};
+
+export default Upload;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use:'raw-loader',
+    });
 
-module.exports = nextConfig
+    return config;
+  }
+};
+
+module.exports = nextConfig;

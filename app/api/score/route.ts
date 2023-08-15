699f98f25dec61374f0a2a7a8b060d81c8a6f44e
Jan Clarin <me@janclarin.com>
2023-08-15 00:43:14 -0700

    Generate and return score

--- app/api/score/route.ts
+++ app/api/score/route.ts
@@ -4,7 +4,7 @@ import { PdfReader } from "pdfreader";
 const parsePdf = async (pdfFile: File) => {
   const resumeFileBuffer = Buffer.from(await pdfFile.arrayBuffer());
   let pdfText = "";
-  return new Promise((resolve, reject) => {
+  return new Promise<string>((resolve, reject) => {
     new PdfReader({}).parseBuffer(resumeFileBuffer, (err, item) => {
       if (err) {
         reject(err);
@@ -17,6 +17,36 @@ const parsePdf = async (pdfFile: File) => {
   });
 };
 
+const generateScore = (resumeText: string, jobDescription: string) => {
+  // TODO: Remove common unimportant english prepositions, e.g. "to", "and", etc.
+  const jobDescriptionKeywords = jobDescription
+    .replaceAll(/\s+/g, " ")
+    .split(" ");
+
+  // TODO: Remove this when we use OCR to read the resume text instead.
+  // Removes whitespace because pdf reader often adds whitespace in random places.
+  const whitespaceRemovedResumeText = resumeText.replaceAll(/\s+/g, "");
+
+  const matchedKeywords = jobDescriptionKeywords.reduce<string[]>(
+    (matches, keyword) => {
+      if (whitespaceRemovedResumeText.includes(keyword)) {
+        return [...matches, keyword];
+      }
+      return matches;
+    },
+    [],
+  );
+
+  const score = Math.round(
+    (matchedKeywords.length / jobDescriptionKeywords.length) * 100,
+  );
+
+  return {
+    matchedKeywords,
+    score,
+  };
+};
+
 export async function POST(request: Request) {
   const formData = await request.formData();
   const jobDescription = formData.get("jobDescription");
@@ -25,6 +55,12 @@ export async function POST(request: Request) {
   if (!jobDescription) {
     return NextResponse.json(
       { error: "Missing job description" },
+      { status: 400 },
+    );
+  }
+  if (typeof jobDescription !== "string") {
+    return NextResponse.json(
+      { error: "Job description is not a string" },
       { status: 400 },
     );
   }
@@ -42,6 +78,6 @@ export async function POST(request: Request) {
   }
 
   const resumeText = await parsePdf(resumeFile);
-
-  return NextResponse.json({ resumeText, jobDescription }, { status: 200 });
+  const { matchedKeywords, score } = generateScore(resumeText, jobDescription);
+  return NextResponse.json({ matchedKeywords, score }, { status: 200 });
 }

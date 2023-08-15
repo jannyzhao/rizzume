8ade56a8b1c1126201666ff4f866f5c9ad05714a
Nishant Racherla <nishant.racherla@gmail.com>
2023-08-14 23:06:03 -0700

    Add redirect on navbar logo click

---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/images/logo.png";
+import { useRouter } from "next/navigation";
+
+const Navbar = () => {
+  const router = useRouter();
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" sx={{ zIndex: 2000 }}>
+        <Toolbar
+          sx={{
+            backgroundColor: "background.paper",
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Link href="/">
+            <Image src={logo} alt="Logo" width={150} className="select-none" />
+          </Link>
+          <Button variant="outlined" color="primary">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navbar;

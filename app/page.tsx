58d405f46f890a32cf734d7b4e679e735a5b7a0b
Janny Zhao <zhao.janny@gmail.com>
2023-08-14 19:19:18 -0700

    Start on User Auth

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,17 @@ import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 
-interface HomeProps {} // You can define props here if needed
-
-const Home: React.FC<HomeProps> = () => {
+export default function Home() {
   return (
     <Container className="flex items-center justify-center h-screen">
       <div>
         <Typography variant="h2">
           How Much Rizz Does Your Resume Have?
+        </Typography>
+        <Typography variant="body1" sx={{ color: "gray" }} className="pb-4">
+          Upload resume, match with job descriptions using Rizz Score. Optimize
+          job search, enhance compatibility, and boost chances for success.
+          Streamlined, tailored, effective.
         </Typography>
         <Button variant="outlined" color="primary">
           Get Started - It's free!
@@ -18,6 +21,4 @@ const Home: React.FC<HomeProps> = () => {
       </div>
     </Container>
   );
-};
-
-export default Home;
+}

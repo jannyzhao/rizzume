93f950a772b8cfef049d30b5dc2860126306a0c7
Jan Clarin <me@janclarin.com>
2023-08-15 18:10:48 -0700

    Add job description and resume image to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,27 @@ import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import SignUpButton from "@/components/SignUpButton";
+import Image from "next/image";
+import resume from "@/public/images/resume.png";
+import jobDescription from "@/public/images/job-description.png";
 
 export default function Home() {
   return (
     <Container className="flex items-center justify-center h-screen">
       <div>
-        <Typography variant="h2">
+        <div className="flex gap-4">
+          <Image
+            alt="Resume banner"
+            className="border rounded-md shadow-lg w-1/2"
+            src={jobDescription}
+          />
+          <Image
+            alt="Resume banner"
+            className="border rounded-md shadow-lg w-1/2"
+            src={resume}
+          />
+        </div>
+        <Typography className="mt-8" component="h1" variant="h2">
           How Much Rizz Does Your Resume Have?
         </Typography>
         <Typography variant="body1" sx={{ color: "gray" }} className="pb-4">
